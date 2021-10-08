44feb669acf3b715f0309fe79b2b8e2af0288c40
Joshua Alvarez <joshuaialvarez06@gmail.com>
2021-10-07 17:25:55 -0700

    init commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components/macro';
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+import Bars from '../images/bars.svg';
+
+const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    background: red;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    x-index: 100;
+    position: fixed;
+    width: 100%;
+`
+
+const NavLink = css`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`;
+
+const Logo = styled(Link)`
+    ${NavLink}
+    color: #fff;
+    font-style: italic;
+`;
+
+const MenuBars = styled.i`
+    display: none;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+        background-image: url(${Bars});
+        background-size: contain;
+        height: 40px;
+        width: 40px;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-50%, 25%);
+    }
+`;
+
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: -48px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+`;
+
+const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <Logo to='/'>ELIXR</Logo>
+            <MenuBars />
+            <NavMenu>
+                {menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>)
+                )}
+            </NavMenu>
+            <NavBtn>
+                <Button to='/contact' primary='true'>
+                    Contact Us
+                </Button>
+            </NavBtn>
+        </Nav>
+    )
+}
+
+export default Navbar
